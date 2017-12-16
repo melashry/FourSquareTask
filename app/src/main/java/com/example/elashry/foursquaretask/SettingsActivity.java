a0901fba790b0fa377c57f9a738c2074e0c7667a
elashry <mohamedelashry323@gmail.com>
2017-12-16 09:54:24 +0200

    intial

---
+++ app/src/main/java/com/example/elashry/foursquaretask/SettingsActivity.java
@@ -0,0 +1,29 @@
+package com.example.elashry.foursquaretask;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.elashry.foursquaretask.Fragment.SettingsFragment;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        getFragmentManager().beginTransaction().replace(R.id.settingsFragment,
+                new SettingsFragment()).commit();
+    }
+}
